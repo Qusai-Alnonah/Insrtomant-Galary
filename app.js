68dc2cfb0a57597ac7eb482d7792fc125beb8fe1
Qusai-Alnonah <kusaialnona@yahoo.com>
2021-03-25 15:45:41 +0200

    add java code

---
+++ app.js
@@ -0,0 +1,56 @@
+  //showin amasage 
+ alart("hello world");
+ Comment 
+ /*
+ DataTypes ;
+ -string an texet or characteres
+ number 
+ boolean
+variables :some to holld or store values
+ */
+"hello";
+'qusai'
+var fistName="Qusai";
+var mynumber= 26;
+var lastname= "Alnonah "
+ console.log(firstName+lastname)
+ //Comparision operarors
+ //Greater thane>
+ //less than<
+ 
+ var x= 20;
+ var y= '20';
+ console.log(x==y);
+         console.log(x===y);
+console.log(15=='15')
+console.log(15==='15')
+
+prompt("What is you name?");
+var username =prompt('What is yur naume');
+console.log(username);
+var secanduser= prompt('')
+ doccument.wri
+DOC
+
+var happy = true
+
+if(happy==false){
+    console.log('sorry for you')
+}else if(happy==true){
+    console.log('its logged')
+}
+else{
+    alart('wow grat')
+}
+var favariteinstrament= 
+prompt('what is ffavareat instrament','piano,valinte,guitar');
+if(favariteinstrament==='guitar'){
+    document.write('<imag scr="">')
+}else if(favariteinstrament===='PIANO'){
+    document.write('<imag scr="">')
+}else if(favariteinstrament==='vaioline'){
+    document.write('<imag scr="">')
+}else  {
+    alart('Wlecome')
+}
+var usarename = prompt('whatis your name?')
